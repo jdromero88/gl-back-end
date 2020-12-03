7863d5afb90b31cc854e77ab55cb9c560c4fb316
jdromero88 <jdromerov88@gmail.com>
2020-12-03 14:31:20 -0500

    Updated GL-lists-controller

--- app/controllers/grocery_list_details_controller.rb
+++ app/controllers/grocery_list_details_controller.rb
@@ -1,2 +1,15 @@
 class GroceryListDetailsController < ApplicationController
+  def index
+    @groceryListDetails = GroceryListDetail.all
+    return render json: @groceryListDetails
+  end
+
+  def create
+    @groceryListDetail = GroceryListDetail.create!(
+      quantity: params[:quantity],
+      grocery_lists_id: params[:grocery_lists_id],
+      product_id: params[:product_id]
+    )
+  end
+
 end
