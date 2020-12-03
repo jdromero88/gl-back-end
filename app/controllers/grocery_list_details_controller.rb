12c373364c47a50b45db71a8ba13f9ada2af67ef
jdromero88 <jdromerov88@gmail.com>
2020-12-03 16:58:05 -0500

    Updated GL-lists-controller

--- app/controllers/grocery_list_details_controller.rb
+++ app/controllers/grocery_list_details_controller.rb
@@ -13,4 +13,31 @@ class GroceryListDetailsController < ApplicationController
     return render json: @groceryListDetail
   end
 
+  def show
+    @groceryListDetail = GroceryListDetail.find(params[:id])
+    if @groceryListDetail
+      return render json: @groceryListDetail
+    end
+    return render json: {
+      error: true,
+      message: "Grocery List Details not found."
+    }
+  end
+
+  def update
+    @groceryListDetail = GroceryListDetail.find(params[:id])
+    if @groceryListDetail
+      @groceryListDetail.update!(
+        quantity: params[:quantity],
+        product_id: params[:product_id]
+      )
+      return render json: @groceryListDetail
+    end
+    return render json: {
+      error: true,
+      message: "Grocery List Details not found."
+    }
+  end
+
+  
 end
